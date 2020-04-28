fa7773e1f9e4928a588493c209d68aac000604ff
gdyekman <gerald.dykeman@gmail.com>
2020-04-28 12:33:56 -0400

    first example - design pattern

---
+++ singleton.py
@@ -0,0 +1,23 @@
+class ConfigValues:
+
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        if ConfigValues.__instance == None:
+            ConfigValues()
+        return ConfigValues.__instance
+    
+    def __init__(self):
+        """ Virtually Private Constructor """
+        if ConfigValues.__instance != None:
+            raise Exception("This class is a singleton")
+        else:
+            ConfigValues.__instance = self
+
+s = ConfigValues.getInstance()
+print(s)
+
+s = ConfigValues.getInstance()
+print(s)
+# s = ConfigValues()
